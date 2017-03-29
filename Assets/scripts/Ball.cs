0d50d63c1a77d5c4dd0c9d07bd8e4762bcfcc3d5
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2017-03-30 05:07:27 +0900

    [add] success!

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.name == "Stage") {
+			Thread.Sleep (3000);
+			transform.position = new Vector3 (1.34f,6f,2.25f);
+		}
+
+		if (c.name == "Cubef") {
+			transform.position = new Vector3 (1.34f,6f,2.25f);
+			GameObject.FindGameObjectWithTag ("Particle").GetComponent<ParticleSystem>().Play ();
+		}
+
+	}
+}
--- Assets/scripts/Move.cs
+++ Assets/scripts/Move.cs
@@ -11,14 +11,13 @@ public class Move : MonoBehaviour {
 	int right = 0;
 	int left = 0;
 	private int b = 0;
-	string text;
 
-//	public static string url = "http://127.0.0.1:4567/";
 	public static string url = "http://192.168.3.8:4567/";
 	private UnityWebRequest request;
 
 	void Start()
 	{
+//		GameObject.FindGameObjectWithTag ("Particle").GetComponent<ParticleSystem>().Play ();
 		StartCoroutine(GetBalance());
 	}
 
@@ -30,18 +29,15 @@ public class Move : MonoBehaviour {
 
 			left = Int32.Parse (splited [0]);
 			right = Int32.Parse (splited [1]);
-			GameObject.FindGameObjectWithTag ("Text").GetComponent<TextMesh> ().text = www.text;
-
-//			Thread.Sleep (10);
 		}
 	}
 	void FixedUpdate () {
 
 		GameObject[] objects = GameObject.FindGameObjectsWithTag ("Mov");
-		GameObject camera = GameObject.FindGameObjectWithTag ("Cam");
+//		GameObject camera = GameObject.FindGameObjectWithTag ("Cam");
 //		GameObject wall = GameObject.Find("Wall");
 
-		int i = (left - right);
+		int i = (int)((left - right) * 0.7);
 		if (i > 20)  i = 20;
 		if (i < -20) i = -20;
 		int r = i - b;
@@ -51,10 +47,10 @@ public class Move : MonoBehaviour {
 			g.transform.Rotate(new Vector3(0f,0f,(float) r));
 		}	
 
-		int t = right - left;
-		float o = ((3 / 20) * t);
+//		int t = right - left;
+//		float o = ((3 / 20) * t);
 
-		camera.transform.localPosition = new Vector3 ((float)o,camera.transform.position.y,camera.transform.position.z);
+//		camera.transform.localPosition = new Vector3 ((float)o,camera.transform.position.y,camera.transform.position.z);
 //		camera.transform.LookAt(wall.transform);
 	}
 }
