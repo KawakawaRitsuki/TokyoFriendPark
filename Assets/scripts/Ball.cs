d3a642a3fdd1f4c1bc4c0ecbf34ad989f3ca0059
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2017-04-05 22:38:33 +0900

    [fix] 通り抜けバグetc...

--- Assets/scripts/Ball.cs
+++ Assets/scripts/Ball.cs
@@ -5,26 +5,38 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	public bool flag = false;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		GetComponent<Rigidbody> ().WakeUp ();
+		Vector3 v = GetComponent<Rigidbody> ().velocity;
 	}
 
 	void OnTriggerEnter(Collider c){
 		if (c.name == "Stage") {
-			Thread.Sleep (3000);
-			transform.position = new Vector3 (1.34f,6f,2.25f);
+			transform.position = new Vector3 (1.34f,5.7f,1.993f);
+			Move.flag = true;
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0f,0f,0f);
 		}
 
 		if (c.name == "Cubef") {
-			transform.position = new Vector3 (1.34f,6f,2.25f);
+			transform.position = new Vector3 (1.34f,5.7f,1.993f);
 			GameObject.FindGameObjectWithTag ("Particle").GetComponent<ParticleSystem>().Play ();
+			Move.flag = true;
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0f,0f,0f);
 		}
+	}
 
+	void OnCollisionEnter(Collision c){
+		if (c.collider.name == "Mov1") {
+//			flag = true;
+			Move.flag = false;
+		}
 	}
 }
--- Assets/scripts/Move.cs
+++ Assets/scripts/Move.cs
@@ -12,12 +12,14 @@ public class Move : MonoBehaviour {
 	int left = 0;
 	private int b = 0;
 
-	public static string url = "http://192.168.3.8:4567/";
+	public static bool flag = true;
+
+	public static string url = "http://192.168.1.7:4567/";
 	private UnityWebRequest request;
 
 	void Start()
 	{
-//		GameObject.FindGameObjectWithTag ("Particle").GetComponent<ParticleSystem>().Play ();
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 		StartCoroutine(GetBalance());
 	}
 
@@ -25,32 +27,34 @@ public class Move : MonoBehaviour {
 		while (true) {
 			WWW www = new WWW(url);
 			yield return www;
-			string[] splited = www.text.Split (',');
+			if(www.text != null && www.text != ""){
+				string[] splited = www.text.Split (',');
 
-			left = Int32.Parse (splited [0]);
-			right = Int32.Parse (splited [1]);
+				left = Int32.Parse (splited [0]);
+				right = Int32.Parse (splited [1]);
+			}
 		}
 	}
+
 	void FixedUpdate () {
-
 		GameObject[] objects = GameObject.FindGameObjectsWithTag ("Mov");
-//		GameObject camera = GameObject.FindGameObjectWithTag ("Cam");
-//		GameObject wall = GameObject.Find("Wall");
 
-		int i = (int)((left - right) * 0.7);
+		int i = (int)((left - right) * 0.5);
 		if (i > 20)  i = 20;
 		if (i < -20) i = -20;
+//		if (flag) i = 2;
 		int r = i - b;
-		b = i;
+
+		if (r > 2)
+			r = 2;
+
+		if (r < -2)
+			r = -2;
+		
+		b = r + b;
 
 		foreach (GameObject g in objects) {
 			g.transform.Rotate(new Vector3(0f,0f,(float) r));
 		}	
-
-//		int t = right - left;
-//		float o = ((3 / 20) * t);
-
-//		camera.transform.localPosition = new Vector3 ((float)o,camera.transform.position.y,camera.transform.position.z);
-//		camera.transform.LookAt(wall.transform);
 	}
 }
